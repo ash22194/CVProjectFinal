80d734214841d82da19d13fac8a5515fc5a66b03
Anahita Mohseni Kabir <anahitam70@gmail.com>
2016-12-02 15:02:35 -0500

    apply gaussian filter on the inpainted video

--- code/testInpainting.m
+++ code/testInpainting.m
@@ -1,15 +1,19 @@
 %% test video
-fileName_prefix = '../Inpainting/videos_';
-fileName_postfix = '_inpainted.jpg';
-load('videos.mat')
-nfiles = length(imagefiles);    % Number of files found
-startFrame = 1;
-stepFrame = 10;
-for ii=startFrame:nfiles
-    image = imread(strcat(strcat(fileName_prefix,num2str((ii-1)*stepFrame+1)),fileName_postfix));
+
+BOUNDARY = 10;
+boundaryPixels = zeros(size(frame,1),size(frame,2));
+boundaryPixels(1:BOUNDARY,:) = 1; boundaryPixels(:,1:BOUNDARY) = 1; 
+boundaryPixels(size(boundaryPixels,1)-BOUNDARY:size(boundaryPixels,1),:) = 1; 
+boundaryPixels(:,size(boundaryPixels,2)-BOUNDARY:size(boundaryPixels,2)) = 1;
+maskConstant = boundaryPixels;
+for lll=[1:size(frames,3)-1]    
     subplot 121
-    imshow(image)
+    imshow(motionInpaint(:,:,lll));
     subplot 122
-    imshow(inpaintVid(:,:,(ii-1)*stepFrame+1))
+    frame = frames(:,:,lll);
+    newFrame = imgaussfilt(frame,1);%%wiener2(frame,[2 2],0.025);    
+    frame(logical(maskConstant)) = newFrame(logical(maskConstant));
+    imshow((frame))
+    pause(0.1) 
 end
 
